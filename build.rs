8290661ef294d0ea5cf73aef98edaa767b0e5c34
Paul English <paulnglsh@gmail.com>
2026-04-08 17:27:11 -0600

    Only try to link libtorch if we're using the tch feature

--- build.rs
+++ build.rs
@@ -1,14 +1,20 @@
+use std::env;
 
 fn main() {
-    let os = std::env::var("CARGO_CFG_TARGET_OS").expect("Unable to get TARGET_OS");
-    match os.as_str() {
-        "linux" | "windows" => {
-            if let Some(lib_path) = std::env::var_os("DEP_TCH_LIBTORCH_LIB") {
-                println!("cargo:rustc-link-arg=-Wl,-rpath={}", lib_path.to_string_lossy());
+    // When we're building with the tch-rs feature
+    if env::var_os("CARGO_FEATURE_TCH").is_some() {
+        let os = env::var("CARGO_CFG_TARGET_OS").expect("Unable to get TARGET_OS");
+        match os.as_str() {
+            "linux" | "windows" => {
+                // Make sure we configure the binary to link libtorch
+                // correctly
+                if let Some(lib_path) = std::env::var_os("DEP_TCH_LIBTORCH_LIB") {
+                    println!("cargo:rustc-link-arg=-Wl,-rpath={}", lib_path.to_string_lossy());
+                }
+                println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
+                println!("cargo:rustc-link-arg=-ltorch");
             }
-            println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
-            println!("cargo:rustc-link-arg=-ltorch");
+            _ => {}
         }
-        _ => {}
     }
 }
